d56f151898bd324ab38bf90fa18dcd9b721a4dac
wdl1111111111111111111111 <2467081156@qq.com>
2021-12-13 23:49:16 +0800

    1

--- 001.py
+++ 001.py
@@ -1,10 +1,16 @@
 import os
 import sys
-def reverse(num):
-    a=int(num/100)
-    b=int(num%100/10)
-    c=int(num%10)
-    return c*100+b*10+a
+import pdb
+
+class Aclass():
+    def __init__(self,num):
+        self.num=num
+    def reverse(self):
+        a = int(self.num / 100)
+        b = int(self.num % 100 / 10)
+        c = int(self.num % 10)
+        return c * 100 + b * 10 + a
+
 
 if __name__ == '__main__':
-    print(reverse(396))
+    print(Aclass(586).reverse())
